13c80e24b2d1509a741a8f8e8cf431c6d7c32ec4
Adam Wegrzynek <adam@wegrzynek.com>
2025-01-06 17:18:04 +0100

    Add rigtorp::SPSCQueue to benchmark

--- gbench_queue.cxx
+++ gbench_queue.cxx
@@ -1,7 +1,13 @@
 #include "WfQueue.cxx"
+
 #ifdef WFQUEUE_WITH_BOOST
 #include <boost/lockfree/spsc_queue.hpp>
 #endif
+
+#ifdef WFQUEUE_WITH_SPSCQUEUE
+#include <rigtorp/SPSCQueue.h>
+#endif
+
 #include <thread>
 #include <iostream>
 #include <benchmark/benchmark.h>
@@ -12,9 +18,11 @@ constexpr auto cpu2 = 2;
 constexpr auto iters = 400'000'000l;
 constexpr auto queueSize = 65536;
 
-#ifdef WFQUEUE_WITH_BOOST
-using boost::lockfree::spsc_queue;
-#endif
+template<typename T>
+struct isRigtorp : std::false_type {};
+
+template<typename ValueT>
+struct isRigtorp<rigtorp::SPSCQueue<ValueT>> : std::true_type {};
 
 template<template<typename> class QueueT>
 void BENCHMARK_queue(benchmark::State &state) {
@@ -26,12 +34,24 @@ void BENCHMARK_queue(benchmark::State &state) {
       // pop warmup
       for (std::int_fast64_t i = 0; i < queueSize; ++i) {
         std::int_fast64_t val = 0;
-        while (auto again = not queue.pop(val)) benchmark::DoNotOptimize(again);
+        if constexpr(isRigtorp<queue_type>::value) {
+          while (not queue.front()) ;
+          benchmark::DoNotOptimize(val = *queue.front());
+          queue.pop();
+        } else {
+          while (auto again = not queue.pop(val)) benchmark::DoNotOptimize(again);
+        }
       }
 
       for (std::int_fast64_t i = 0; i < iters; ++i) {
         std::int_fast64_t val = 0;
-        while (auto again = not queue.pop(val)) benchmark::DoNotOptimize(again);
+        if constexpr(isRigtorp<queue_type>::value) {
+          while (not queue.front()) ;
+          benchmark::DoNotOptimize(val = *queue.front());
+          queue.pop();
+        } else {
+          while (auto again = not queue.pop(val)) benchmark::DoNotOptimize(again);
+        }
         if (val != i) {
           throw std::runtime_error("Value does not match");
         }
@@ -42,13 +62,21 @@ void BENCHMARK_queue(benchmark::State &state) {
     pinThread(cpu2);
     // push warmup
     for (std::int_fast64_t i = 0; i < queueSize; ++i) {
-      while (auto again = not queue.push(i)) benchmark::DoNotOptimize(again);
+      if constexpr(isRigtorp<queue_type>::value) {
+        while (auto again = not queue.try_push(i)) benchmark::DoNotOptimize(again);
+      } else {
+        while (auto again = not queue.push(i)) benchmark::DoNotOptimize(again);
+      }
     }
     while (auto again = not queue.empty()) benchmark::DoNotOptimize(again);
 
     std::int_fast64_t i = 0;
     for (; i < iters; ++i) {
-      while (auto again = not queue.push(i)) benchmark::DoNotOptimize(again);
+      if constexpr(isRigtorp<queue_type>::value) {
+        while (auto again = not queue.try_push(i)) benchmark::DoNotOptimize(again);
+      } else {
+        while (auto again = not queue.push(i)) benchmark::DoNotOptimize(again);
+      }
     }
     while (auto again = not queue.empty()) benchmark::DoNotOptimize(again);
 
@@ -60,7 +88,11 @@ void BENCHMARK_queue(benchmark::State &state) {
 BENCHMARK_TEMPLATE(BENCHMARK_queue, WfQueue);
 
 #ifdef WFQUEUE_WITH_BOOST
-BENCHMARK_TEMPLATE(BENCHMARK_queue, spsc_queue);
+BENCHMARK_TEMPLATE(BENCHMARK_queue, boost::lockfree::spsc_queue);
+#endif
+
+#ifdef WFQUEUE_WITH_SPSCQUEUE
+BENCHMARK_TEMPLATE(BENCHMARK_queue, rigtorp::SPSCQueue);
 #endif
 
 BENCHMARK_MAIN();
